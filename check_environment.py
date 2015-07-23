9da086fbd2935fde3916a6338cad216c9b2fd48d
Tom Augspurger <tom.w.augspurger@gmail.com>
2015-07-23 07:47:25 -0500

    Now really add housing

--- check_environment.py
+++ check_environment.py
@@ -13,4 +13,8 @@ else:
     if len(bad) > 0:
         print('\n'.join(bad))
     else:
+        from sklearn.datasets import california_housing
+        print("Caching california_housing")
+        data = california_housing.fetch_california_housing()
         print("All good. Enjoy the tutorial!")
+
